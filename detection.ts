5e1887a95ab29f58fe16ce80ddb9ff73d92930cf
Radhey Mugdal <mugdalradhey@gmail.com>
2024-03-07 17:57:03 +0530

    learned is and instance of

--- detection.ts
+++ detection.ts
@@ -15,3 +15,30 @@ function provideId(id:string |null){
     }
     return id.toLowerCase()
 }
+
+
+//instance of
+
+function logvalue(x: Date  |string){
+    if(x instanceof Date){
+        return x
+    }
+}
+
+type Fish={
+    swin :()=>void
+}
+type Bird={
+    fly:()=>void
+}
+function  isFish(pet:Fish|Bird) :pet is Fish {
+    return (pet as Fish ).swin === undefined 
+}
+
+function getFood(pet : Fish | Bird){
+    if(isFish(pet)){
+        pet
+        return "fishfood"
+    }
+    return "birdfood"
+}
